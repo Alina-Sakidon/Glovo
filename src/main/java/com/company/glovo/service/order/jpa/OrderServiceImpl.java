d5113db7aa5cb71fe4539fde791a66d6851689e8
asakidon <asakidon@corcentric.com>
2024-02-08 17:33:59 +0200

    add changes OrderServiceImpl

--- src/main/java/com/company/glovo/service/order/jpa/OrderServiceImpl.java
+++ src/main/java/com/company/glovo/service/order/jpa/OrderServiceImpl.java
@@ -2,21 +2,24 @@ package com.company.glovo.service.order.jpa;
 
 import com.company.glovo.dto.OrderDto;
 import com.company.glovo.service.OrderService;
+
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
 
+
+    @NonNull
     private List<OrderDto> orders;
 
     @Override
     public OrderDto getOrderById(Integer id) {
-        return orders.stream().filter(o -> o.getId().equals(id)).findFirst().orElse(null);
+        return orders.stream().filter(o -> o.getId().equals(id)).findFirst().orElseThrow(NullPointerException::new);
     }
 
     @Override
@@ -26,20 +29,20 @@ public class OrderServiceImpl implements OrderService {
 
     @Override
     public void saveOrder(OrderDto dto) {
-        orders = new ArrayList<>();
         orders.add(dto);
     }
 
     @Override
     public void updateOrder(Integer id, OrderDto dto) {
-        orders.stream().filter(o -> o.getId().equals(id))
-                .map(f -> new OrderDto(dto.getId(), dto.getDate(), dto.getCost(), dto.getProducts()));
+        orders.stream().filter(o -> o.getId().equals(id)).findFirst()
+                .map(f -> new OrderDto(dto.getId(), dto.getDate(), dto.getCost(), dto.getProducts()))
+                .orElseThrow(NullPointerException::new);
     }
 
     @Override
     public void deleteOrder(Integer id) {
-        List<OrderDto> operatedList = new ArrayList<>();
-        orders.stream().filter(o -> o.getId().equals(id)).forEach(operatedList::add);
-        orders.removeAll(operatedList);
+        OrderDto order = orders.stream().filter(o -> o.getId().equals(id)).findFirst()
+                .orElseThrow(NullPointerException::new);
+        orders.remove(order);
     }
 }
