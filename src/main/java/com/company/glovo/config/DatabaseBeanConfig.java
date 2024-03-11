d95b4efda327ed1b7517536e0360ca7f8fe62370
asakidon <asakidon@corcentric.com>
2024-03-11 12:49:37 +0200

    add default profile data

--- src/main/java/com/company/glovo/config/DatabaseBeanConfig.java
+++ src/main/java/com/company/glovo/config/DatabaseBeanConfig.java
@@ -1,21 +1,16 @@
 package com.company.glovo.config;
 
 import com.zaxxer.hikari.HikariDataSource;
-import lombok.extern.slf4j.Slf4j;
-import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 
 import javax.sql.DataSource;
-import java.util.logging.Logger;
 
 
 @Configuration
 public class DatabaseBeanConfig {
 
-  /*  @Bean
+    @Bean
     public DataSource dataSource() {
         HikariDataSource dataSource = new HikariDataSource();
         dataSource.setDriverClassName("org.postgresql.Driver");
@@ -24,6 +19,5 @@ public class DatabaseBeanConfig {
         dataSource.setPassword("dtrm65");
 
         return dataSource;
-    }*/
-
+    }
 }
